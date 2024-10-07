8dfdda637dc1f86d7ae68069bc10f96408ca1b38
Yoonhojoon <dbsghwns1209@gmail.com>
2024-10-08 03:24:03 +0900

    버전 오류 해결 ㅡㅡ

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, PSDraw
 from pathlib import Path
 
 # 업로드된 이미지 저장 경로
@@ -8,7 +8,7 @@ UPLOAD_DIR.mkdir(exist_ok=True)
 # 숫자에 대응하는 메시지를 설정하는 부분 (필요시 추가 가능)
 def process_image(image_file: str, number: str):
     # 사용자로부터 입력받은 숫자를 찾음
-    text = "123123"  # 없는 숫자는 입력받은 숫자로 처리
+    text = "12526123"  # 없는 숫자는 입력받은 숫자로 처리
 
     # 웹캠 이미지를 Pillow로 열기
     img = Image.open(image_file)
@@ -23,35 +23,33 @@ def process_image(image_file: str, number: str):
     scale_factor = 6
     new_width = int(original_width * scale_factor)
     new_height = int(original_height * scale_factor)
+    size = (new_width,new_height)
 
-    # 비율 유지하며 이미지 크기 조정
-    img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    # # 비율 유지하며 이미지 크기 조정
+    img = img.resize((new_width, new_height))
 
-    # 배경 디자인 이미지 열기 (PNG 파일이 투명도를 지원함)
-    background = Image.open("./uploads/frame.png").convert("RGB")  # JPG는 투명도 없음, RGB로 변환
+    # # 배경 디자인 이미지 열기 (PNG 파일이 투명도를 지원함)
+    background = Image.open("./uploads/frame.png") # JPG는 투명도 없음, RGB로 변환
+    img_size = background.size
 
-    # 웹캠 이미지를 배경 디자인 위에 합성
+    # # 웹캠 이미지를 배경 디자인 위에 합성
     background.paste(img, (620, 527))  # 특정 위치에 웹캠 이미지 붙여넣기
 
-    # 텍스트 추가 (숫자 삽입, 커스텀 폰트 사용)
+   
+    # # 텍스트 추가 (숫자 삽입, 커스텀 폰트 사용)
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype("./uploads/DepartureMono-Regular.otf", 400)  # Departure Mono 폰트 사용
 
-    # 텍스트 크기 계산
-    text_width, text_height = draw.textsize(text, font=font)
+    bbox = draw.textbbox((0, 0), text, font=font)
+    text_width, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    position = ((img_size[0] - text_width) // 2, 3852)
 
-    # 배경 이미지 크기
-    bg_width, bg_height = background.size
+    # # 텍스트 추가 (세로 위치는 고정)
+    draw.text(position, text,align="center", font=font, fill=(0, 0, 0))  # 원하는 위치에 텍스트 삽입
 
-    # 텍스트의 위치 계산 (가로 중앙)
-    text_x = (bg_width - text_width) // 2
-
-    # 텍스트 추가 (세로 위치는 고정)
-    draw.text((text_x, 3852), text, font=font, fill=(0, 0, 0))  # 원하는 위치에 텍스트 삽입
-
-    # 결과 이미지를 저장 (JPG 형식)
-    output_path = UPLOAD_DIR / "output.jpg"
-    background.save(output_path, "JPEG")  # JPG로 저장
+    # # 결과 이미지를 저장 (JPG 형식)
+    output_path = UPLOAD_DIR / "output.png"
+    background.save(output_path, "png")  # JPG로 저장
 
     print(f"Image processed successfully. Output saved at: {output_path}")
 
