3c503109bcf1c141402f829b4044435f56f9f551
dbsghwns <dbsghwns1209@gmail.com>
2024-10-25 18:23:38 +0900

    최종 화면 합성까지 완료. 리액트로 인코딩해서 넘기는 것만 확인하면 됨

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import shutil
 import time
 import base64
+from buzzer import activate_buzzer
 
 app = FastAPI()
 
@@ -12,7 +13,7 @@ UPLOAD_DIR = Path("uploads/")
 UPLOAD_DIR.mkdir(exist_ok=True)
 
 # 이미지 처리 함수
-def process_image(image_file: str, number: str):
+def process_image(image_file: str, number: str, message:str):
     # 사용자로부터 입력받은 숫자를 처리
     text = number  # 사용자가 입력한 숫자를 텍스트로 사용
 
@@ -26,43 +27,53 @@ def process_image(image_file: str, number: str):
     original_width, original_height = img.size
 
     # 이미지 크기 비율을 유지하면서 확대할 크기 설정 (예: 6배 확대)
-    scale_factor = 1.52
+    scale_factor = 1.5
     new_width = int(original_width * scale_factor)
     new_height = int(original_height * scale_factor)
     size = (new_width, new_height)
 
-    # 비율 유지하며 이미지 크기 조정
-    img = img.resize((new_width, new_height))
+    # # 배경 디자인 이미지 열기 (PNG 파일이 투명도를 지원함)
+    background = Image.open("./uploads/frame.png") # JPG는 투명도 없음, RGB로 변환
+    img_size = background.size
 
-    # 배경 디자인 이미지 열기 (PNG 파일이 투명도를 지원함)
-    background = Image.open("./uploads/frame.png").convert('RGB')  # 배경 이미지
+    # # 웹캠 이미지를 배경 디자인 위에 합성
+    background.paste(img, (159, 213))  # 특정 위치에 웹캠 이미지 붙여넣기
 
-    # 웹캠 이미지를 배경 디자인 위에 합성
-    background.paste(img, (155, 146))  # 특정 위치에 웹캠 이미지 붙여넣기
-
-    # 텍스트 추가 (숫자 삽입, 커스텀 폰트 사용)
+    # # 텍스트 추가 (숫자 삽입, 커스텀 폰트 사용)
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype("./uploads/DepartureMono-Regular.otf", 94)  # Departure Mono 폰트
+    font = ImageFont.truetype("./uploads/DepartureMono-Regular.otf", 100)  # Departure Mono 폰트 사용
 
-    # 텍스트 크기 계산
     bbox = draw.textbbox((0, 0), text, font=font)
     text_width, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
-    position = ((background.size[0] - text_width) // 2, 940)
+    position = ((img_size[0] - text_width) // 2, 1078)
 
-    # 텍스트 추가 (세로 위치는 고정)
-    draw.text(position, text, align="center", font=font, fill=(0, 0, 0))  # 텍스트 삽입
+    # # 텍스트 추가 (세로 위치는 고정)
+    draw.text(position, text,align="center", font=font, fill=(0, 0, 0))  # 원하는 위치에 텍스트 삽입
 
-    # 날짜 텍스트 추가
+     #ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ날짜 합성ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 2.38베
     text_date = "2024.11.02"
-    font_date = ImageFont.truetype("./uploads/Galmuri14.ttf", 47)
+    font_date = ImageFont.truetype("./uploads/Galmuri14.ttf", 45)
 
-    # 날짜 텍스트 크기 계산
+    # 텍스트 크기 계산
     bbox = draw.textbbox((0, 0), text_date, font=font_date)
-    date_width, date_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
-    position_date = ((background.size[0] - date_width) // 2, 1227)
 
-    # 날짜 텍스트 추가
-    draw.text(position_date, text_date, align="center", font=font_date, fill=(0, 0, 0))
+    date_width, date_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    position_date = (596, 862)
+
+    draw.text(position_date, text_date, align="center", font=font_date, fill=(255,255,255))
+
+    #ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ메시지 합성ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    text_date = message
+    font_date = ImageFont.truetype("./uploads/DungGeunMo.ttf", 73)
+
+    # 텍스트 크기 계산
+    bbox = draw.textbbox((0, 0), text_date, font=font_date)
+
+    date_width, date_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    position_date = ((img_size[0] - date_width) // 2, 1257)
+
+    draw.text(position_date, text_date, align="center", font=font_date, fill=(0,0,0))
+
 
     # 결과 이미지를 저장 (PNG 형식)
     output_path = UPLOAD_DIR / "output.jpg"
@@ -78,7 +89,9 @@ def process_image(image_file: str, number: str):
 
 # FastAPI 라우팅 부분
 @app.post("/process-image/")
-async def process_image_api(file: UploadFile = File(...), number: str = Form(...)):
+async def process_image_api(
+    file: UploadFile = File(...), number: str = Form(...), message: str = Form(...)
+):
     # 사용자로부터 받은 웹캠 사진 저장
     image_path = UPLOAD_DIR / file.filename
     with image_path.open("wb") as buffer:
@@ -86,14 +99,19 @@ async def process_image_api(file: UploadFile = File(...), number: str = Form(...
 
     # 이미지 처리 함수 호출
     start_time = time.time()
-    encoded_image = process_image(image_path, number)
+    encoded_image = process_image(image_path, number, message)
     end_time = time.time()
 
     return {
         "message": "Image processed successfully.",
-        "output_image": encoded_image,  # Base64 인코딩된 이미지 데이터
+        "output_image": encoded_image,
         "processing_time": end_time - start_time
     }
+
+@app.post("/activate-buzzer/")
+async def activate_buzzer_api():
+    activate_buzzer()
+    return {"message": "Buzzer activated successfully"}
 
 @app.get("/")
 #async def root():
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont, PSDraw
 from pathlib import Path
 import time
+from buzzer import activate_buzzer
 
 # 업로드된 이미지 저장 경로
 UPLOAD_DIR = Path("uploads/")
@@ -15,13 +16,13 @@ def process_image(image_file: str, number: str):
     img = Image.open(image_file)
 
     # 사진 1대1 비율로 자르기
-    img = img.crop((0, 0, 480, 480))
+    img = img.crop((0, 0, 720, 720))
 
     # 이미지의 현재 크기
     original_width, original_height = img.size
 
     # 이미지 크기 비율을 유지하면서 확대할 크기 설정 (예: 6배 확대)
-    scale_factor = 6
+    scale_factor = 1
     new_width = int(original_width * scale_factor)
     new_height = int(original_height * scale_factor)
     size = (new_width,new_height)
@@ -34,31 +35,44 @@ def process_image(image_file: str, number: str):
     img_size = background.size
 
     # # 웹캠 이미지를 배경 디자인 위에 합성
-    background.paste(img, (620, 527))  # 특정 위치에 웹캠 이미지 붙여넣기
+    background.paste(img, (159, 213))  # 특정 위치에 웹캠 이미지 붙여넣기
 
    
     # # 텍스트 추가 (숫자 삽입, 커스텀 폰트 사용)
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype("./uploads/DepartureMono-Regular.otf", 400)  # Departure Mono 폰트 사용
+    font = ImageFont.truetype("./uploads/DepartureMono-Regular.otf", 100)  # Departure Mono 폰트 사용
 
     bbox = draw.textbbox((0, 0), text, font=font)
     text_width, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
-    position = ((img_size[0] - text_width) // 2, 3852)
+    position = ((img_size[0] - text_width) // 2, 1078)
 
     # # 텍스트 추가 (세로 위치는 고정)
     draw.text(position, text,align="center", font=font, fill=(0, 0, 0))  # 원하는 위치에 텍스트 삽입
 
      #ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ날짜 합성ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 2.38베
     text_date = "2024.11.02"
-    font_date = ImageFont.truetype("./uploads/Galmuri14.ttf", 400)
+    font_date = ImageFont.truetype("./uploads/Galmuri14.ttf", 45)
 
     # 텍스트 크기 계산
     bbox = draw.textbbox((0, 0), text_date, font=font_date)
 
     date_width, date_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
-    position_date = ((img_size[0] - date_width) // 2, 4000)
+    position_date = (596, 862)
+
+    draw.text(position_date, text_date, align="center", font=font_date, fill=(255,255,255))
+
+    #ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ메시지 합성ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    text_date = "안녕안녕"
+    font_date = ImageFont.truetype("./uploads/DungGeunMo.ttf", 73)
+
+    # 텍스트 크기 계산
+    bbox = draw.textbbox((0, 0), text_date, font=font_date)
+
+    date_width, date_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    position_date = ((img_size[0] - date_width) // 2, 1257)
 
     draw.text(position_date, text_date, align="center", font=font_date, fill=(0,0,0))
+
 
 
     # # 결과 이미지를 저장 (JPG 형식)
@@ -66,6 +80,7 @@ def process_image(image_file: str, number: str):
     background.save(output_path, "png")  # JPG로 저장
 
     print(f"Image processed successfully. Output saved at: {output_path}")
+    activate_buzzer()
 
 # 테스트할 이미지 파일 경로와 숫자 입력
 test_image_file = 'uploads/webcam.png'  # 웹캠으로 찍은 이미지 파일
